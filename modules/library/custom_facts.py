ed3b2a6e56359e442efcf03ac638430758af1591
Ваше Матвиенко Юлия Алексеевна <uliamatvienko74670@gmail.com>
2025-12-12 09:58:03 +0400

    feat: Кастомные модули и дополнительные плейбуки

---
+++ modules/library/custom_facts.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from ansible.module_utils.basic import AnsibleModule
+import platform
+import socket
+
+def run_module():
+    module = AnsibleModule(argument_spec={})
+    
+    facts = {
+        'lab_facts': {
+            'environment': 'Ansible Laboratory',
+            'hostname': socket.gethostname(),
+            'platform': platform.platform(),
+            'python_version': platform.python_version()
+        }
+    }
+    
+    result = dict(
+        changed=False,
+        facts=facts
+    )
+    
+    module.exit_json(**result, ansible_facts=facts)
+
+if __name__ == '__main__':
+    run_module()
---
+++ modules/library/custom_info.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from ansible.module_utils.basic import AnsibleModule
+import datetime
+
+def run_module():
+    module_args = dict(
+        student_name=dict(type='str', required=True),
+        lab_number=dict(type='int', default=1)
+    )
+    
+    result = dict(
+        changed=False,
+        message='',
+        timestamp=''
+    )
+    
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+    
+    student_name = module.params['student_name']
+    lab_number = module.params['lab_number']
+    
+    result['message'] = f"Лабораторная работа {lab_number} выполнена студентом {student_name}"
+    result['timestamp'] = datetime.datetime.now().isoformat()
+    result['student'] = student_name
+    result['lab'] = lab_number
+    
+    module.exit_json(**result)
+
+if __name__ == '__main__':
+    run_module()
